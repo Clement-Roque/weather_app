f51e298b4e3c389656ecfb704bf307b0f5a486d0
clem <clement.eberoque@gmail.com>
2021-01-13 08:48:02 +0100

    functions is working but unconsistenly, making test flaky

--- test/weather/services/weatherservice_test.py
+++ test/weather/services/weatherservice_test.py
@@ -3,7 +3,15 @@ from weather.services.weatherservice import WeatherService
 class TestWeatherService(object):
 
     def test_weather_service(self) -> None:
-        weather_service = WeatherService("https://meteofrance.com/previsions-meteo-france/")
+        weather_service = WeatherService("https://meteofrance.com/previsions-meteo-france/montpellier/34000")
 
         assert isinstance(weather_service, WeatherService)
-        assert weather_service.url == "https://meteofrance.com/previsions-meteo-france/"
+        assert weather_service.url == "https://meteofrance.com/previsions-meteo-france/montpellier/34000"
+
+    def test_get_weather(self) -> None:
+
+        weather_service = WeatherService("https://meteofrance.com/previsions-meteo-france/montpellier/34000")
+
+        temperature: int = weather_service.get_temperature()
+
+        assert isinstance(temperature, int)
--- weather/services/weatherservice.py
+++ weather/services/weatherservice.py
@@ -1,6 +1,21 @@
+from selenium import webdriver  # type: ignore
+
+
 class WeatherService(object):
-    """docstring for WeatherService"""
 
     def __init__(self, url: str):
         super(WeatherService, self).__init__()
         self.url = url
+
+    def get_temperature(self) -> int:
+
+        browser: webdriver.WebDriver = webdriver.Firefox()
+        browser.get(self.url)
+
+        weather_temperature_from_browser: webdriver.WebElement = browser.find_element_by_class_name("temp").text
+
+        browser.quit()
+
+        weather_temperature = int(weather_temperature_from_browser.replace('°', '', 1))
+
+        return weather_temperature
