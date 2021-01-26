c99f07ff4ca79005ab83f0fd4d318db73fde3b0a
clem <clement.eberoque@gmail.com>
2021-01-26 08:55:16 +0100

    Disable static checking due to biase with ABC
    As Mypy doesn't allow us to instanciate an Abstract Class, we need to
    explicitly ignore these steps from type checking, because the facade
    implementation is legit.

--- weather/services/weatherservicefacade.py
+++ weather/services/weatherservicefacade.py
@@ -1,14 +1,11 @@
 from typing import List, Dict
 from weather.services.weatherservice import WeatherService
-# from weather import services
-
 
 class WeatherServiceFacade(object):
 
     def __init__(self) -> None:
         super(WeatherServiceFacade, self).__init__()
-        self.weather_services: List[WeatherService] = [
-            weather_service_cls for weather_service_cls in WeatherService.__subclasses__()]
+        self.weather_services: List[WeatherService] = WeatherService.__subclasses__()  # type: ignore
 
     def get_weather_data(self) -> Dict[str, int]:
 
@@ -18,8 +15,11 @@ class WeatherServiceFacade(object):
         # try catch every services call and the global facade get wether behavior with custom weatherewception
         for weather_service_cls in self.weather_services:
 
-            weather_service: WeatherService = weather_service_cls()
-            weather_data = weather_service.get_weather_data()
-            return weather_data
+            try:
+                weather_service: WeatherService = weather_service_cls()  # type: ignore
+                weather_data = weather_service.get_weather_data()
+                return weather_data
+            except Exception:
+                pass
 
         return weather_data
