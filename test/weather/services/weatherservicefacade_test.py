d38ac417121684bde8bcce567e9f6df929d66a5f
clem <clement.eberoque@gmail.com>
2021-01-26 08:57:59 +0100

    Disable connection for testing empty response
    We want to check the behavior of the facade in an no internet error
    case.
    We try to cut the socket manually, but i will consider to use
    https://pypi.org/project/pytest-socket/ instead

--- test/weather/services/weatherservicefacade_test.py
+++ test/weather/services/weatherservicefacade_test.py
@@ -1,10 +1,19 @@
+import socket
+import pytest
+
 from weather.services.weatherservicefacade import WeatherServiceFacade
 from weather.services.weatherservice import WeatherService
 from test.weather.schemas.data.weather_schemas import weather_schema_fields
 
+
+@pytest.fixture
+def guard_fixture() -> None:
+    def guard() -> Exception:
+        raise Exception("I told you not to use the Internet!")
+
 class TestWeatherServiceFacade(object):
 
-    def test_weather_service_facade_get_weather_data(self) -> None:
+    def test_weather_service_facade(sefl) -> None:
 
         weather_service_facade: WeatherServiceFacade = WeatherServiceFacade()
 
@@ -12,10 +21,30 @@ class TestWeatherServiceFacade(object):
 
         for weather_service in weather_service_facade.weather_services:
 
-            assert isinstance(weather_service, WeatherService)
+            assert isinstance(weather_service(), WeatherService)  # type: ignore
+
+    def test_weather_service_facade_get_weather_data(self) -> None:
+
+        weather_service_facade: WeatherServiceFacade = WeatherServiceFacade()
 
         facade_weather_data = weather_service_facade.get_weather_data()
 
         assert isinstance(facade_weather_data, dict)
 
         assert weather_schema_fields == list(facade_weather_data.keys())
+
+    def test_weather_service_facade_dont_get_weather_data(self, guard_fixture) -> None:
+
+        original_socket: socket.SocketType = socket.socket
+
+        socket.socket = guard_fixture  # type: ignore
+
+        weather_service_facade: WeatherServiceFacade = WeatherServiceFacade()
+
+        facade_weather_data = weather_service_facade.get_weather_data()
+
+        assert isinstance(facade_weather_data, dict)
+
+        assert facade_weather_data == {}
+
+        socket.socket = original_socket
